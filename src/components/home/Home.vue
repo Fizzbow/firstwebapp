217194d79573baf9d7b90f1ac95d519f8dd2ed94
Fizzbow <541375716@qq.com>
2021-10-04 21:45:34 +0800

    add Location

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,18 +3,21 @@
       <home-header></home-header>
       <home-swiper></home-swiper>
       <home-icons></home-icons>
+      <home-location></home-location>
   </div>
 </template>
 <script>
 import HomeHeader from './pages/Header'
 import HomeSwiper from './pages/Swiper'
 import HomeIcons from './pages/Icons'
+import HomeLocation from './pages/Location'
 
 export default {
   components:{
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeLocation
   }
 }
 </script>
---
+++ src/components/home/pages/Location.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="location border-top">
+      <div class="location-item border-right">
+        <span class="iconfont">&#xe677</span>
+          北京大兴
+      </div>
+    <div class="location-item">
+        <span class="iconfont">&#xe811</span>
+        玩水记
+      </div>
+  </div>
+</template>
+<script></script>
+<style>
+.location{
+  display: flex;
+  position: relative;
+}
+.location .location-item{
+  flex: 1;
+  font-size: .28rem;
+  text-align: center;
+  line-height: 1rem;
+}
+</style>
