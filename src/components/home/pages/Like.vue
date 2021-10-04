cad8df3472dddfca3a6b70e537b86997972f3f38
Fizzbow <541375716@qq.com>
2021-10-04 23:41:55 +0800

    add Like

---
+++ src/components/home/pages/Like.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="like">
+    <div class="like-top">
+      <span class="love">💕</span>
+      猜你喜欢
+    </div>
+
+    <ul>
+      <li class="like-item border-bottom">
+        <div class="like-img">
+          <img src="https://imgs.qunarzz.com/p/p97/1512/73/97da2a9e39df59f7.jpg_256x160_acb41adf.jpg">
+        </div>
+        <div>
+          <div>
+            1314条评论
+          </div>
+          <div>
+            <span>￥<b>18.8</b></span>
+            <span>平谷区</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script></script>
+<style scoped>
+.like{
+  margin-top: .2rem;
+  font-size: .28rem;
+  background: #fff;
+  padding: 0 .2rem;
+}
+.like-top{
+  padding: 0.2rem 0;
+  font-size: .32rem;
+}
+.like-list{
+  position: relative;
+  padding: 0.2rem 0;
+  overflow: hidden;
+}
+.like-img{
+  float: left;
+}
+.like-img img{
+  width: 2rem;
+  height: 2rem;
+}
+.like-item{
+  position: relative;
+  overflow: hidden;
+}
+</style>
