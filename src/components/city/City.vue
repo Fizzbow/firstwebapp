d84993204593a7328b22f998316708afd725c217
Fizzbow <541375716@qq.com>
2021-10-05 14:58:19 +0800

    create city

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,45 +1,19 @@
 <template>
-  <div class="header">
-    <div class="header-left" @click ='toHome'>
-      <span class="iconfont">
-        &#xe743;
-      </span>
-    </div>
-    <div class="header-title">
-      城市选择
-    </div>
+  <div class="city">
+    <city-header></city-header>
   </div>
 </template>
 <script>
+import CityHeader from './pages/Header'
+
 export default {
-  methods:{
-    toHome(){
-      this.$router.push('/')
-    }
+  components:{
+    CityHeader
   }
 }
 </script>
-<style scoped lang="stylus">
-@import "~css/var.styl"
-.header{
-  width: 100%;
-  line-height: .88rem;
-  background: $bgColor;
-  color: #fff;
-  font-size: .36rem;
-  position relative;
-
+<style scoped>
+.city{
+  background:#f5f5f5 ;
 }
-.header-left{
-  width: .4rem;
-  padding: 0 .2rem;
-  text-align: center;
-  font-weight: bold;
-  position absolute;
-}
-  .header-title{
-    text-align center;
-    font-size 1rem;
-  }
-
 </style>
---
+++ src/components/city/pages/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <div class="header-left" @click ='toHome'>
+      <span class="iconfont">
+        &#xe743;
+      </span>
+    </div>
+    <div class="header-title">
+      城市选择
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    toHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+<style scoped lang="stylus">
+@import "~css/var.styl"
+.header{
+  width: 100%;
+  line-height: .88rem;
+  background: $bgColor;
+  color: #fff;
+  font-size: .36rem;
+  position relative;
+
+}
+.header-left{
+  width: .4rem;
+  padding: 0 .2rem;
+  text-align: center;
+  font-weight: bold;
+  position absolute;
+}
+.header-title{
+  text-align center;
+  font-size .4rem;
+}
+
+</style>
