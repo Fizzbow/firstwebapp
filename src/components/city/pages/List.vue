250e4b37a317dfc8f688af318a1d14fc8e44175e
Fizzbow <541375716@qq.com>
2021-10-06 10:24:38 +0800

    add List

---
+++ src/components/city/pages/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="list">
+    <div class="list-title">A</div>
+    <ul class="list-msg">
+      <li class="list-item">阿克苏地区</li>
+      <li class="list-item">阿克苏地区</li>
+      <li class="list-item">阿克苏地区</li>
+      <li class="list-item">阿克苏地区</li>
+      <li class="list-item">阿克苏地区</li>
+      <li class="list-item">阿呗藏族自治区</li>
+    </ul>
+  </div>
+</template>
+<script></script>
+<style scoped lang="stylus">
+@import "~css/var.styl"
+.list-title{
+  font-size: .24rem;
+  color: #212121;
+  padding: .2rem .3rem;
+}
+.list-msg{
+  position: relative;
+  background: #fff;
+  overflow: hidden;
+}
+.list-msg:before{
+  content: '';
+  left: 25%;
+  width: 25%;
+  height: 100%;
+  position: absolute;
+  border-left: .03rem solid #ddd;
+  border-right: .03rem solid #ddd;
+}
+.list-msg:after{
+  content: '';
+  position: absolute;
+  left: 75%;
+  width: 25%;
+  height: 100%;
+  border-left: .02rem solid #ddd;
+}
+.list-item{
+  textOverflow();
+  width: 25%;
+  font-size: .28rem;
+  text-align: center;
+  line-height: .9rem;
+  float: left;
+  border-bottom: .03rem solid #ddd;
+}
+
+</style>
--- src/components/city/pages/Sort.vue
+++ src/components/city/pages/Sort.vue
@@ -1,36 +1,35 @@
 <template>
-  <div class="hot">
-    <div class="hot-title">字母排序</div>
-    <ul class="hot-list">
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
-      <li class="hot-item">A</li>
+  <div class="sort">
+    <div class="sort-title">字母排序</div>
+    <ul class="sort-list">
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
+      <li class="sort-item">A</li>
     </ul>
   </div>
 </template>
 <script></script>
 <style scoped>
-.hot-title{
+.sort-title{
   font-size: .24rem;
   color: #212121;
   padding: .2rem .3rem;
 }
-.hot-list{
+.sort-list{
   position: relative;
   overflow: hidden;
   background: #fff;
   font-size: .28rem;
 }
-.hot-item{
+.sort-item{
   width: 16.66666%;
   text-align: center;
   height: .9rem;
