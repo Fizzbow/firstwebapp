1596b4350c42d2f6e6915993cd591f783706313f
Fizzbow <541375716@qq.com>
2021-10-06 12:35:03 +0800

    add scroll

--- src/components/city/pages/List.vue
+++ src/components/city/pages/List.vue
@@ -1,20 +1,62 @@
 <template>
-  <div class="list">
-    <div v-for="(val,key) in cities">
-      <div class="list-title">{{ key }}</div>
-      <ul class="list-msg">
-        <li class="list-item"  v-for="item in val" :key="item.id">{{ item.name }}</li>
-      </ul>
+  <div ref="container" class="container">
+    <div>
+      <div class="list">
+        <div v-for="(val,key) in cities">
+          <div class="list-title">{{ key }}</div>
+          <ul class="list-msg">
+            <li class="list-item"  v-for="item in val" :key="item.id">{{ item.name }}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="hot">
+        <div class="hot-title">热门城市</div>
+        <ul class="hot-list">
+          <li class="hot-item"
+              v-for="item in hotCities"
+              :key="item.id"
+          >{{ item.name }}</li>
+        </ul>
+      </div>
+      <div class="sort">
+        <div class="sort-title">字母排序</div>
+        <ul class="sort-list">
+          <li class="sort-item"
+              v-for="(val,key) in cities"
+          >
+            {{key}}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  props:['cities']
+  props:['cities','hotCities'],
+  data(){
+    return{
+      scroll:''
+    }
+  },
+  mounted() {
+    let container = this.$refs['container']
+    this.scroll = new BScroll(container)
+  }
 }
 </script>
 <style scoped lang="stylus">
 @import "~css/var.styl"
+.container{
+  position absolute;
+  left 0;
+  right 0;
+  bottom 0;
+  top .88rem;
+  overflow hidden;
+  background #f5f5f5;
+}
 .list-title{
   font-size: .24rem;
   color: #212121;
@@ -50,6 +92,52 @@ export default {
   line-height: .9rem;
   float: left;
   border-bottom: .03rem solid #ddd;
+}
+.hot-title{
+  font-size: .24rem;
+  color: #212121;
+  padding: .2rem .3rem;
+}
+.hot-list{
+  position: relative;
+  overflow: hidden;
+  background: #fff;
+  font-size: .28rem;
+}
+.hot-list:before{
+  content: '';
+  position: absolute;
+  height: 100%;
+  width: 33.33333%;
+  left: 33.3333%;
+  border-left: .03rem solid #ddd;
+  border-right: .03rem solid #ddd;
+}
+.hot-item{
+  width: 33.33333%;
+  text-align: center;
+  height: .9rem;
+  line-height: .9rem;
+  float: left;
+  border-bottom: .02rem solid #ddd;
+}
+.sort-title{
+  font-size: .24rem;
+  color: #212121;
+  padding: .2rem .3rem;
+}
+.sort-list{
+  position: relative;
+  overflow: hidden;
+  background: #fff;
+  font-size: .28rem;
+}
+.sort-item{
+  width: 16.66666%;
+  text-align: center;
+  height: .9rem;
+  line-height: .9rem;
+  float: left;
 }
 
 </style>
